3fc7b6662115aa332c45592cac065d4563e78010
Dhanasri <dhanasreepothineni@gmail.com>
2022-02-08 16:58:40 +0530

    Added One to one mapping

---
+++ src/main/java/com/example/demo/entity/Component.java
@@ -0,0 +1,78 @@
+package com.example.demo.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Component {
+     @Id
+     @GeneratedValue(strategy = GenerationType.AUTO)
+     private Integer componentid;
+     private String componentname;
+     private String description;
+     private String suppliers;
+     private String productname;
+     
+     /*@OneToOne(mappedBy ="component")
+ 	 Product product; */
+
+     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+     @JoinColumn(name = "product_id", nullable = false)
+     private Product product;
+
+
+	public Integer getComponentid() {
+		return componentid;
+	}
+	
+	
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+	public void setComponentid(Integer componentid) {
+		this.componentid = componentid;
+	}
+	public String getComponentname() {
+		return componentname;
+	}
+	public void setComponentname(String componentname) {
+		this.componentname = componentname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSuppliers() {
+		return suppliers;
+	}
+	public void setSuppliers(String suppliers) {
+		this.suppliers = suppliers;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+}
---
+++ src/main/java/com/example/demo/service/ComponentService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Component;
+import com.example.demo.repository.ComponentRepository;
+
+
+@Service
+public class ComponentService {
+        
+	@Autowired
+	ComponentRepository componentRepository;
+	
+	
+	
+	public void saveComponent(Component component) {
+		componentRepository.save(component);
+		
+	}
+
+	public Iterable<Component> getComponent() {
+		return componentRepository.findAll();
+	}
+
+}
