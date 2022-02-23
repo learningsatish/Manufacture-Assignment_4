2b176c485d1a235b9f6ac7e22b147bc4606cabde
learningsatish <satishlearning@gmail.com>
2022-02-23 20:22:37 +0530

    First Commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,5 +1,69 @@
 package com.example.demo.entity;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.FetchMode;
+import org.hibernate.annotations.Fetch;
+
+@Entity
 public class Product {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productid;
+    private String productname;
+    private String quantity;
+   
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    private Set<Component> components;
+    
+    
+    
+    public Set<Component> getComponents() {
+		return components;
+	}
+	public void setComponents(Set<Component> components) {
+		this.components = components;
+	}
+	/*@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="component_id", referencedColumnName = "componentid")
+	Component component;
+	
+	public Component getComponent() {
+		return component;
+	}
+	public void setComponent(Component component) {
+		this.component = component;
+	}  */
+	public Integer getProductid() {
+		return productid;
+	}
+	public void setProductid(Integer productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
 
 }
